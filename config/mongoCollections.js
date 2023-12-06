4e87297c811f39d30ef04a0e904d3072592c662f
wmckim <wmckim@stevens.edu>
2023-12-05 21:31:29 -0500

    Ported config files

---
+++ config/mongoCollections.js
@@ -0,0 +1,18 @@
+import {dbConnection} from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT
+export const users = getCollectionFn('users');
+export const posts = getCollectionFn('posts');
